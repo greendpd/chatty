cc0014a181f13959676cbe51c972684baafa2f61
greendpd <greendpd@msn.com>
2017-08-01 15:33:50 -0500

    basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const messages=[];
+// const messages = [{
+//     to: 'Mr. Grinch',
+//     from: 'Whovilles',
+//     body: 'you\'re a mean one, mr grinch'
+//   },
+//   {
+//     to: 'potus',
+//     from: 'liberals',
+//     body: 'you\'re racist'
+//   },
+//   {
+//     to: 'aliens',
+//     from: 'tom delonge',
+//     body: 'pls send proof'
+//   },
+//   {
+//     to: 'Adele\'s momma',
+//     from: 'Adele',
+//     body: 'hello'
+//   }
+// ];
 
+app.listen(3000, function() {
+  console.log("Yo ho, yo ho, a pirate's life for me");
+})
+
+app.use(express.static('assets'))
+app.use(bodyParser.json());
+
+app.get('/messages', function(req, res, next) {
+  console.log(req.body);
+  res.status(200).json({
+    messages: messages
+  })
+})
+
+app.post('/messages',function(req,res,next){
+  messages.push({message: req.body.message, time: new Date()});
+  console.log(req.body);
+  res.status(200).json({messages:messages})
+})
+
+
+
+
+
+
+
+
+//
